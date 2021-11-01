983a807a8db0ac6061354a4c7935cacef2142d0a
luzinsan <53607329+Luzinsan@users.noreply.github.com>
2021-11-01 13:48:16 +0700

    Update VT_4_luzinsan.cpp

--- VT_4_luzinsan.cpp
+++ VT_4_luzinsan.cpp
@@ -14,46 +14,89 @@
 Подсчитать количество таких пар чисел X и У, что (Х * У – (X div 2)) = 0. Ответ вывести на экран.*/
 int main()
 {
-    unsigned count = 0;
-    signed _int16 X, Y;
+    _int16 count = 0; 
+    signed _int8 X, Y;
+    signed _int8 X_r, Y_r;
+    bool flag = false;
+    const char* msg = "\n\tThe number of pairs is less than 200";
     _asm 
     {
         
-        mov X, 126 // X = [-128; 127]
+        mov X, -128 // X = [-128; 127]
         M1:
-           
-
-            mov Y, 126 // Y = [-128; 127]
+            
+            mov Y, -128 // Y = [-128; 127]  
             M2:
-                mov bx, 2; // для (X / ->_2_)
-                mov ax, word ptr[X] // X(byte) 
-                mov edx, dword ptr[X] // для (X % 2)
                 
-                
-                imul [Y] // ax * Y(word) => dx:ax
-                push ax // сохраняем (X * Y)
-                mov eax, edx // X
-                idiv bx // AX = EAX / BX
-                
-                mov bx, ax // bx = (X % 2)
-                pop ax // pop (X * Y)
-                sub ax, bx // (Х * У) – (X div 2)
+                mov al, X   // для (->_X_ div    2 )
+                cbw
+                mov dl, 2;           // для (   X  div ->_2_)
+                idiv dl             // (even part)AL = AX / DL---> AL (X div 2)
+                cbw
+                // сохраняем (X div 2)
+                push ax     // --------------------------> AX (X div 2)
+               
+                mov al, X // X(byte) 
+                cbw
+                imul Y // AL * Y(byte) => AX
+                pop dx // pop (X div 2)
+                //sub ax, dx
+                sub al, dl // (Х * У) – (X div 2)
+                sbb ah, dh
                 cmp ax, 0
                 jne M3
                     inc count
-                    
+                    cmp count, 200
+                    jne M3
+                        cmp flag, 1
+                        je M3
+                            mov flag, 1
+                            mov al, X
+                            mov X_r, al
+                            mov al, Y
+                            mov Y_r, al
                 M3:
-
                 
-                inc Y
-            cmp Y, 127
-            jle M2
+            inc Y
+            cmp Y, -128
+                jne M2
               
-            
-            inc X
-        cmp X, 127
-        jle M1
+        inc X
+        cmp X, -128
+        jne M1
+
     }
-    std::cout << count;
+
+    std::cout << "\n\tNumber of pairs: " << static_cast<int>(count);
+    if (flag)
+        std::cout << "\n\t200-th pair\n\t ( x = " << static_cast<int>(X_r) << "; y = " << static_cast<int>(Y_r) << ' )' << std::endl;
+    else
+        std::cout << msg;
+    
+
+
+
+
+    bool tflag = false;
+    int counter = 0, RX, RY;
+    for (int iii = -128; iii < 128; ++iii)
+        for (int jjj = -128; jjj < 128; ++jjj)
+            if ((iii * jjj - (iii / 2)) == 0)
+            {
+                counter++;
+                std::cout << "\n\tThe "<< counter <<"-th pair\t(x = " << iii << "; y = " << jjj << ')' << std::endl;
+                if (counter == 200 && tflag == false)
+                {
+                    tflag = true;
+                    RX = iii; RY = jjj;
+                }
+            }
+
+    std::cout << "\n\tNumber of pairs: " << counter;
+    if (tflag)
+        std::cout << "\n\t200-th pair\n\t ( x = " << RX << "; y = " << RY << ')' << std::endl;
+    else
+        std::cout << msg;
+
     return 0;
 }
